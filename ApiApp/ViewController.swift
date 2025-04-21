ed09ee4d956c84b9d8b4ffdbd0fdbabb0cc5f69a
YoshinoriHashizume <yoshinori_hashizume@marueikogyo.jp>
2025-04-21 15:13:48 +0900

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by Yoshinori Hashizume on 2025/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
